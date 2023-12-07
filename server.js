b45beca7fa88b96af5c2568781baa5cf72a95dd2
Hariom <82692072+DevHarry066@users.noreply.github.com>
2023-12-07 08:50:40 -0800

    Route setup in Express

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ const app = express();
 PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`App Running in ${process.env.NODE_ENV} on PORT ${PORT}`));
+
+app.get('/api/v1/bootcamp', (req, res) => {
+	res.status(200).json({ success: true, message: "Show All bootcamps"});
+});
+
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+	res.status(200).json({ success: true, message: `Show bootcamp of id ${req.params.id}`});
+});
+
+app.post('/api/v1/bootcamp', (req, res) => {
+	res.status(200).json({ success: true, message: "Created new bootcamps"});
+});
+
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+	res.status(200).json({ success: true, message: `update bootcamp of ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+	res.status(200).json({ success: true, message: `delete bootcamp of ${req.params.id}` });
+});

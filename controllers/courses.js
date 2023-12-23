9e68f3a2e61397ac5097836c6d2c7ae515d2d468
Hariom <82692072+DevHarry066@users.noreply.github.com>
2023-12-23 09:48:11 -0800

    Update and Delete Courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -53,3 +53,32 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     const course = await Course.create(req.body);
     res.status(201).json({ success: true, data: course });
 });
+
+// @desc Update Course
+// @route PUT /api/v1/courses/:id
+// access Public
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 400));
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    res.status(200).json({ success: true, data: course });
+});
+
+// @desc Delete Course
+// @route Delete /api/v1/courses/:id
+// access Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findByIdAndDelete(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 400));
+    }
+    res.status(200).json({ success: true, data: {} });
+});

abe89d1b20620395dfc9abf6ca970984b3a1b21f
Hariom <82692072+DevHarry066@users.noreply.github.com>
2023-12-22 23:51:26 -0800

    Select & Sort with query params

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,10 +8,45 @@ const geocoder = require('../utils/geocoder');
 // access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryString = JSON.stringify(req.query);
+
+  //copy query string
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  //Loop over all fields & remove fields from queryParameters
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  //create query string
+  let queryString = JSON.stringify(reqQuery);
+
+  //create operators ($gt, $lte, $gte, $lt, $in)
   queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  //Finding resources
   query = Bootcamp.find(JSON.parse(queryString));
+
+  //Select Fields by query string values
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  //Sort Fields by query string values
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    //default sort by creation time
+    // query = query.sort({ createdAt: -1 });
+  }
+
+  //Finally send the query to the server
   const bootcamps = await query;
+
+  //Result from the server to the client
+
   if (!bootcamps) {
     return next(new ErrorResponse(`Bootcamps not found`, 400));
   }
